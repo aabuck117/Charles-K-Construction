436040811bd91d1b40b645cd6c61082fb1ded9f9
aabuck117 <aabuck117@gmail.com>
2026-04-06 10:57:24 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/YOUR-REPO-NAME/', // <-- THIS IS THE FIX
   plugins: [
     tailwindcss(),
   ],
